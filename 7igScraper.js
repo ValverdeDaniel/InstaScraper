5c307de7f87d84fbb617d36fd16fad2fe97e8c3c
Daniel Valverde <DValverde@uniphyaco.com>
2019-12-19 11:51:33 -0700

    finished 7igScraper is the winner

--- 7igScraper.js
+++ 7igScraper.js
@@ -3,21 +3,29 @@ const request = require('request-promise');
 const { getMetadata } = require('page-metadata-parser');
 const domino = require('domino');
 
-const igImgId = 'B6GOuCsAUkU';
+const igImgId = 'B6G1gislVyQ';
 //const igImgId = 'B6GOuCsAUkU';
 const BASE_URL = `https://www.instagram.com/p/${igImgId}/`;
+let username;
 // / Create the base function to be ran /
 (async () => {
+try {
+    let html = await request(BASE_URL);
+    const doc = domino.createWindow(html).document;
+    const metadata = getMetadata(doc, BASE_URL);
+    if (metadata != null && metadata.description != null) {
+      username=metadata.description.match(/\(([^)]+)\)/)[1];
+      console.log('metadata is', metadata.description.match(/\(([^)]+)\)/)[1]);
+      console.log(username);
+    } else {
+      console.log('either metadata is undefined or it does not contains the description name')
+    }
+    debugger;
+} catch {
+    console.log('something went wrong with the scraper probably that multiphoto for private user scenario')
+} finally {
+    console.log('outside try: ' + username)
+}
 
-  let html = await request(BASE_URL);
-  const doc = domino.createWindow(html).document;
-  const metadata = getMetadata(doc, BASE_URL);
-  if (metadata != null && metadata.description != null) {
-    let username=metadata.description.match(/\(([^)]+)\)/)[1];
-    console.log('metadata is', metadata.description.match(/\(([^)]+)\)/)[1]);
-    console.log(username);
-  } else {
-    console.log('either metadata is undefined or it does not contains the description name')
-  }
-  debugger;
 })() 
+
